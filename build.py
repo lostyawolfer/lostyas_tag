d41ae2f0ecafa2692a91896e721dfeb7d99f10f1
thodnev <thodnev@gmail.com>
2024-12-18 22:17:31 +0100

    feat(res): respack builder

--- build.py
+++ build.py
@@ -11,16 +11,19 @@ import hashlib
 import io
 import logging
 import pathlib as pth
+import types
+import uuid
 import zipfile
 
 log = logging.getLogger(__name__)
 # TODO: Set custom logging format for INFO level
 logging.basicConfig(level=logging.ERROR)
 
-DEFAULTS = {
-    'SERVERPROPS': 'server.properties.in'
-}
-
+DEFAULTS = types.SimpleNamespace(
+    serverprops='server.properties.in',
+    respack_dir='resources',
+    respack_zip='build/respack.zip',
+)
 
 
 def zip_folder(indir, outdir, filename, *, compression_ratio=9):
@@ -65,10 +68,58 @@ def zip_folder(indir, outdir, filename, *, compression_ratio=9):
         return zip.infolist()
         
 def file_hash(file, *, algo='sha1'):
+    needclose = False
+    if not isinstance(file, io.BytesIO):
+        file = open(file, 'rb')
+        needclose = True
     h = hashlib.file_digest(file, algo.lower())
+    if needclose:
+        file.close()
     return h.hexdigest()
 
-def build_config(tpl_file, out_file=None):
+def properties_escape(string):
+    replacements = {        # rely on dict insertion order
+        '\\': '\\\\',       # first escape \ with \\
+        '=': '\\=',
+        ':': '\\:',
+        '\n': '\\u000A',
+    }
+    
+    for orig, new in replacements.items():
+        string = string.replace(orig, new)
+    return string
+
+
+__CFG_FAKE_SECTION = '__FAKE_SECTION__'
+
+def read_config(file):
+    # fake section name
+    data = f'[{__CFG_FAKE_SECTION}]\n'
+    with open(file) as f:
+        data += f.read()
+    
+    cfg = configparser.ConfigParser(
+        comment_prefixes=['#', '!'], delimiters=['=', ':'])
+    cfg.read_string(data)
+    return cfg
+
+def write_config(cfg, file, *, with_spaces=False):
+    buf = io.StringIO()
+    cfg.write(buf, space_around_delimiters=with_spaces)
+    buf.seek(0)     # rewind buffer to initial
+
+    lines = buf.readlines()
+    # strip fake section
+    lines = [line for line in lines if not line == f'[{__CFG_FAKE_SECTION}]\n']
+    
+    with open(file, 'w') as fout:
+        data = ''.join(lines)
+        return fout.write(data)
+
+
+def build_config(tpl_file, out_file=None, entries=None):
+    entries = dict() if entries is None else entries
+
     tpl_file = pth.Path(tpl_file)
 
     # when out_file not provided, generate its name by stripping latest extension
@@ -80,36 +131,60 @@ def build_config(tpl_file, out_file=None):
 
     cfg = read_config(tpl_file)
 
+    section = cfg[__CFG_FAKE_SECTION]
+    # patch entries
+    for k, v in entries.items():
+        section[k] = properties_escape(v) if isinstance(v, str) else v
 
-__CFG_FAKE_SECTION = '[__FAKE_SECTION__]'
+    write_config(cfg, out_file)
 
-def read_config(file):
-    # fake section name
-    data = f'{__CFG_FAKE_SECTION}\n'
-    with open(file) as f:
-        data += f.read()
-    
-    cfg = configparser.ConfigParser(
-        comment_prefixes=['#'], delimiters=['='])
-    cfg.read_string(data)
-    return cfg
+def build_respack_config(
+        respack_zip,            # our archive
+        respack_url,            # url to be formatted
+        respack_prompt=None,         # message
+        respack_uuid=None,
+        *, 
+        tpl_file=DEFAULTS.serverprops):
 
-def write_config(cfg, file, *, with_spaces=False):
-    buf = io.StringIO()
-    cfg.write(buf, space_around_delimiters=with_spaces)
-    buf.seek(0)     # rewind buffer to initial
+    # first get sha1 hashsum for archive
+    sha = file_hash(respack_zip)
 
-    lines = buf.readlines()
-    # strip fake section
-    lines = [line for line in lines if not line == f'{__CFG_FAKE_SECTION}\n']
-    
-    with open(file, 'w') as fout:
-        data = ''.join(lines)
-        return fout.write(data)
+    if respack_uuid is None:        # generate ourselves
+        # use sha1 hashsum as uuid seed
+        seed = f'sha1.{sha}'
+        respack_uuid = uuid.uuid5(uuid.NAMESPACE_OID, seed)
+        respack_uuid = str(respack_uuid)        # need to be hyphenated-uuid-format
 
+    entries = {
+        'resource-pack': respack_url,
+        'resource-pack-id': respack_uuid,
+        'resource-pack-sha1': sha,
+    }
+    if respack_prompt is not None:
+        entries['resource-pack-prompt'] = respack_prompt
+
+    # write it into config using template
+    build_config(tpl_file=tpl_file, out_file=None, entries=entries)
+
+
+def build_respack(respack_url,
+                  respack_dir=DEFAULTS.respack_dir,
+                  zip_fpath=DEFAULTS.respack_zip,
+                  respack_prompt=None,
+                  tpl_file=DEFAULTS.serverprops):
+    """
+    Compress respack_dir into zip_fpath.
+    Then build server.properties for it,
+    assuming respack will be hosted at respack_url.
+    Optional respack_prompt is exactly as used in server.properties directly.
+    """
+    zip_fpath = pth.Path(zip_fpath)
+    # Compress with 0 ratio for faster decompression on clients
+    zip_folder(respack_dir, zip_fpath.parent, zip_fpath.name, compression_ratio=0)
+
+    build_respack_config(zip_fpath, respack_url=respack_url,
+                         respack_prompt=respack_prompt, tpl_file=tpl_file)
 
 if __name__ == '__main__':
     log.setLevel(logging.DEBUG)
-    cfg = read_config(DEFAULTS['SERVERPROPS'])
-    write_config(cfg, 'server.properties')
-    #zip_folder('resources', 'build', 'resources.zip')
+    build_respack(respack_url='https://tofill.later/?my=respack.zip')
