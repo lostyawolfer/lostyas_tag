446a05c9a441f2a8fb4fe3e5432787968faeb8b0
thodnev <thodnev@gmail.com>
2024-12-28 15:58:36 +0100

    refactor(build): zip recursion protection

--- build.py
+++ build.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """Builds server-side and client-side, including assets."""
 
 # TODO:
@@ -11,6 +10,7 @@ import hashlib
 import io
 import logging
 import pathlib as pth
+import sys
 import types
 import uuid
 import zipfile
@@ -26,24 +26,41 @@ DEFAULTS = types.SimpleNamespace(
 )
 
 
-def zip_folder(indir, outdir, filename, *, compression_ratio=9):
+class BuildError(Exception):
+    pass
+
+
+def zip_folder(indir, outfile, *, compression_ratio=9):
     """Archives `indir` into `outdir`/`filename` ZIP.
     Uses provided `compression_ratio`
     """
     assert 0 <= compression_ratio <= 9, 'Compression ratio out of bounds'
-    
-    indir, outdir = map(pth.Path, [indir, outdir])
-    assert indir.is_dir(), 'Input directory does not exist'
-    outdir.mkdir(exist_ok=True)     # ensure output directory exists
 
-    outfile = outdir.joinpath(filename)
+    indir, outfile = map(pth.Path, [indir, outfile])
+    assert indir.is_dir(), 'Input directory does not exist'
+    outdir = outfile.parent
+    outdir.mkdir(exist_ok=True)  # ensure output directory exists
+
+    # check that outfile is not placed under indir
+    # Catch ValueError (meaning our ZIP is not to be placed in subpath of indir)
+    # When no ValueError is raised, it means we have a recursion -> mark FAILURE
+    try:
+        outfile.relative_to(indir)
+    except ValueError:
+        pass  # ignore raised exception
+    else:  # there was no exception
+        msg = f'Cannot put "{outfile}" into directory "{indir}"'
+        msg += ' being archived'
+        raise BuildError(msg)
+
     log.debug(f'Constructed output path: "{outfile}"')
 
     # Windows natively supports only DEFLATE. Also use STORED for uncompressed ZIPs
     algo = zipfile.ZIP_STORED if not compression_ratio else zipfile.ZIP_DEFLATED
-    
-    with zipfile.ZipFile(outfile, mode='w', compression=algo,
-                          compresslevel=compression_ratio) as zip:
+
+    with zipfile.ZipFile(outfile, mode='w',
+                         compression=algo,
+                         compresslevel=compression_ratio) as zip:
         # Walk input directory, putting files in archive
         for dirpath, dirnames, filenames in indir.walk():
             basepth = pth.Path(dirpath).relative_to(indir)
@@ -55,18 +72,19 @@ def zip_folder(indir, outdir, filename, *, compression_ratio=9):
             # create directories in ZIP archive
             for subdir in subdirs:
                 zip.mkdir(str(subdir))
-            
+
             # put files into archive
             for fname in filenames:
                 infile = pth.Path(dirpath).joinpath(fname)
                 arcname = infile.relative_to(indir)
                 log.debug(f'\t\tFile "{arcname}"')
                 zip.write(infile, str(arcname))
-        
+
         assert zip.testzip() is None, 'ZIP integrity failure'
 
         return zip.infolist()
-        
+
+
 def file_hash(file, *, algo='sha1'):
     needclose = False
     if not isinstance(file, io.BytesIO):
@@ -77,6 +95,7 @@ def file_hash(file, *, algo='sha1'):
         file.close()
     return h.hexdigest()
 
+
 def properties_escape(string):
     replacements = {        # rely on dict insertion order
         '\\': '\\\\',       # first escape \ with \\
@@ -84,7 +103,7 @@ def properties_escape(string):
         ':': '\\:',
         '\n': '\\u000A',
     }
-    
+
     for orig, new in replacements.items():
         string = string.replace(orig, new)
     return string
@@ -92,26 +111,28 @@ def properties_escape(string):
 
 __CFG_FAKE_SECTION = '__FAKE_SECTION__'
 
+
 def read_config(file):
     # fake section name
     data = f'[{__CFG_FAKE_SECTION}]\n'
     with open(file) as f:
         data += f.read()
-    
-    cfg = configparser.ConfigParser(
-        comment_prefixes=['#', '!'], delimiters=['=', ':'])
+
+    cfg = configparser.ConfigParser(comment_prefixes=['#', '!'],
+                                    delimiters=['=', ':'])
     cfg.read_string(data)
     return cfg
+
 
 def write_config(cfg, file, *, with_spaces=False):
     buf = io.StringIO()
     cfg.write(buf, space_around_delimiters=with_spaces)
-    buf.seek(0)     # rewind buffer to initial
+    buf.seek(0)  # rewind buffer to initial
 
     lines = buf.readlines()
     # strip fake section
     lines = [line for line in lines if not line == f'[{__CFG_FAKE_SECTION}]\n']
-    
+
     with open(file, 'w') as fout:
         data = ''.join(lines)
         return fout.write(data)
@@ -138,22 +159,23 @@ def build_config(tpl_file, out_file=None, entries=None):
 
     write_config(cfg, out_file)
 
+
 def build_respack_config(
-        respack_zip,            # our archive
-        respack_url,            # url to be formatted
-        respack_prompt=None,         # message
+        respack_zip,  # our archive
+        respack_url,  # url to be formatted
+        respack_prompt=None,  # message
         respack_uuid=None,
-        *, 
+        *,
         tpl_file=DEFAULTS.serverprops):
 
     # first get sha1 hashsum for archive
     sha = file_hash(respack_zip)
 
-    if respack_uuid is None:        # generate ourselves
+    if respack_uuid is None:  # generate ourselves
         # use sha1 hashsum as uuid seed
         seed = f'sha1.{sha}'
         respack_uuid = uuid.uuid5(uuid.NAMESPACE_OID, seed)
-        respack_uuid = str(respack_uuid)        # need to be hyphenated-uuid-format
+        respack_uuid = str(respack_uuid)  # need to be hyphenated-uuid-format
 
     entries = {
         'resource-pack': respack_url,
@@ -180,11 +202,18 @@ def build_respack(respack_url,
     """
     zip_fpath = pth.Path(zip_fpath)
     # Compress with 0 ratio for faster decompression on clients
-    zip_folder(respack_dir, zip_fpath.parent, zip_fpath.name, compression_ratio=0)
+    zip_folder(respack_dir, zip_fpath, compression_ratio=0)
 
-    build_respack_config(zip_fpath, respack_url=respack_url,
-                         respack_prompt=respack_prompt, tpl_file=tpl_file)
+    build_respack_config(zip_fpath,
+                         respack_url=respack_url,
+                         respack_prompt=respack_prompt,
+                         tpl_file=tpl_file)
+
+
+def parse_args(argv=sys.argv):
+    ...
+
 
 if __name__ == '__main__':
     log.setLevel(logging.DEBUG)
-    build_respack(respack_url='https://tofill.later/?my=respack.zip')
+    build_respack(respack_url='https://tofill.later/?my=respack.zip', )
